cf6a43eedd871b29be1fa72556f24d2ad6174d1d
Bobby Montalvo <bmontalvo@movietickets.com>
2017-06-06 16:39:47 -0400

    Fixed Api to play nice with Postman.

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -27,8 +27,9 @@ namespace Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
-            services.AddMvc();
+            services.AddMvcCore()
+                .AddAuthorization()
+                .AddJsonFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
